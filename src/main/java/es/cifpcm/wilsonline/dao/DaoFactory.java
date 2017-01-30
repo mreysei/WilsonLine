07e273ca37b16660292f6363eda05633b92181be
CristinaDev <CristinaDeveloper93@gmail.com>
2017-01-30 21:57:46 +0000

    DaoFactory
    DaoFactory class and ConnectionProvider interface implemented.

---
+++ src/main/java/es/cifpcm/wilsonline/dao/DaoFactory.java
@@ -0,0 +1,60 @@
+package es.cifpcm.wilsonline.dao;
+
+import es.cifpcm.wilsonline.interfaces.ConnectionProvider;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Cristina
+ */
+public class DaoFactory implements ConnectionProvider
+{
+    private static final Logger LOG = LoggerFactory.getLogger(DaoFactory.class);
+    private static DaoFactory instance;
+    private DataSource ds;
+    
+    private DaoFactory()
+    {
+        try
+        {
+            Context ctx = new InitialContext();
+            
+            //We cast the jdbc resource name into a DataSource datatype.
+            this.ds = (DataSource) ctx.lookup("jdbc/flight_reservations");
+        } catch(NamingException ex)
+        {
+            LOG.error(ex.getMessage());
+        }
+    }
+    
+    public synchronized DaoFactory getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new DaoFactory();
+        }
+        
+        return instance;
+    }
+
+    @Override
+    public Connection getConnection()
+    {
+        try
+        {
+            return this.ds.getConnection();
+        } catch(SQLException ex)
+        {
+            LOG.error(ex.getMessage());
+            
+            return null;
+        }
+    }
+}

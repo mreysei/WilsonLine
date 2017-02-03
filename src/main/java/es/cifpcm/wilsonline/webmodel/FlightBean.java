a705c909ddf91ffec1b730455a794029b7db04d6
CristinaDev <CristinaDeveloper93@gmail.com>
2017-02-03 17:22:58 +0000

    FlightBean
    FlightBean generated.

--- src/main/java/es/cifpcm/wilsonline/webmodel/FlightBean.java
+++ src/main/java/es/cifpcm/wilsonline/webmodel/FlightBean.java
@@ -1,7 +1,14 @@
 package es.cifpcm.wilsonline.webmodel;
 
+import es.cifpcm.wilsonline.dao.DaoFactory;
+import es.cifpcm.wilsonline.interfaces.GenericDao;
+import es.cifpcm.wilsonline.model.Flight;
+import java.io.Serializable;
+import java.util.List;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -9,13 +16,26 @@ import javax.enterprise.context.RequestScoped;
  */
 @Named(value = "flightBean")
 @RequestScoped
-public class FlightBean
+public class FlightBean extends Flight implements Serializable
 {
+    private static final Logger LOG = LoggerFactory.getLogger(GenericFlightBean.class);
+    private List<Flight> flights;
+    
     /**
      * Creates a new instance of FlightBean
      */
     public FlightBean()
     {
         
+    }
+    
+    public void search()
+    {
+        if(this.getGenericFlightId() != null)
+        {
+            GenericDao dao = DaoFactory.getInstance().getFlightDao();
+            
+            
+        }
     }
 }
